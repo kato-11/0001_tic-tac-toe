c2f625f3fa214a953a1fb17bdc1edd4547412bb6
kato-11 <katouryo1111@gmail.com>
2023-10-23 21:35:13 +0900

    [update]: 先攻・後攻の選択〜ゲーム開始までの処理を追記

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* ============================================================================
-    まるばつゲーム　：　プレイヤーはまる、CPUはばつ
+    まるばつゲーム：プレイヤーはまる、CPUはばつ
 ============================================================================ */
 const gameBoard = document.getElementById('game_board');
 let messageBox = document.createElement('p');
@@ -14,7 +14,7 @@ let PlayerPlayFirst = true;    // 手番管理の変数。初期値はプレイ�
 
 
 /* ============================================================================
-    ゲーム版のマスを作成（作成後は、非表示にしておく）
+    ゲーム版のマスを作成：作成後は、非表示にしておく
 ============================================================================ */
 for (let x = 0; x < 3; x++) {
     squares[x] = new Array(3);
@@ -22,7 +22,69 @@ for (let x = 0; x < 3; x++) {
         squares[x][y] = document.createElement('div');
         squares[x][y].className = 'square';
         gameBoard.appendChild(squares[x][y]);
+        squares[x][y].style.display = 'none';
     }
+}
+
+
+/* ============================================================================
+    先攻・後攻を選択
+============================================================================ */
+messageBox.className = 'message_box';
+gameBoard.appendChild(messageBox);
+function startSelector() {
+    messageBox.textContent = '先攻・後攻を選択して下さい';
+    for (let i = 0; i < 2; i++) {
+        playFirstSelector[i] = document.createElement('div');
+        playFirstSelector[i].className = 'play_first_selector';
+        playFirstSelector[i].textContent = playFirstSelectorValue[i][0];
+        playFirstSelector[i].style.backgroundColor = playFirstSelectorValue[i][1];
+        gameBoard.appendChild(playFirstSelector[i]);
+        playFirstSelector[i].addEventListener('click', function(event) {
+            if (this.textContent == playFirstSelectorValue[1][0]) {
+                PlayerPlayFirst = false;
+            } else {
+                messageBox.textContent = 'あなたの番です';
+            }
+            gameStart();
+            for (let i = 0; i < 2; i++) {
+                playFirstSelector[i].remove();
+            }
+        }, false);
+    }
+}
+startSelector();
+
+
+/* ============================================================================
+    ゲーム開始：マスの表示およびクリックイベントの設定
+============================================================================ */
+function gameStart() {
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            squares[x][y].style.display = 'block';
+            squares[x][y].addEventListener('click', squareClick);
+        }
+    }
+    if (!(PlayerPlayFirst)) {
+        computerPlayer();
+    }
+}
+
+
+/* ============================================================================
+    マスが押された時の処理：プレイヤーのターン処理
+============================================================================ */
+function squareClick() {
+
+}
+
+
+/* ============================================================================
+    CPUの処理
+============================================================================ */
+function computerPlayer() {
+
 }
 
 
