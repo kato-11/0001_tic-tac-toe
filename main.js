a24c5901fbcbd206906f014a92ba7cf8c9feb012
kato-11 <katouryo1111@gmail.com>
2023-10-23 22:39:19 +0900

    [update]: ゲーム処理〜終了までを追記

--- main.js
+++ main.js
@@ -76,15 +76,162 @@ function gameStart() {
     マスが押された時の処理：プレイヤーのターン処理
 ============================================================================ */
 function squareClick() {
-
+    if (PlayerPlayFirst) {
+        let l = this.classList;
+        if (l.contains('valueO') || l.contains('valueX')) {
+            return;
+        } else {
+            l.add('valueO');
+        }
+        if (!(gameOverCheck())) {
+            PlayerPlayFirst = false;
+            computerPlayer();
+        }
+    }
 }
 
 
 /* ============================================================================
-    CPUの処理
+    CPUの処理：完全ランダムでマスを選択
 ============================================================================ */
 function computerPlayer() {
+    messageBox.textContent = '相手の番です';
+    let tmpFlag = 0;
+    let tmpX = Math.floor(Math.random() * 3);
+    let tmpY = Math.floor(Math.random() * 3);
+    while (tmpFlag == 0) {
+        let e = squares[tmpX][tmpY];
+        if (e.classList.contains('valueO') || e.classList.contains('valueX')) {
+            tmpX = Math.floor(Math.random() * 3);
+            tmpY = Math.floor(Math.random() * 3);
+        } else {
+            e.classList.add('valueX');
+            tmpFlag = 1;
+        }
+    }
+    if (!(gameOverCheck())) {
+        PlayerPlayFirst = true;
+        messageBox.textContent = 'あなたの番です';
+    }
+}
 
+
+/* ============================================================================
+    ゲーム終了のチェック
+============================================================================ */
+function gameOverCheck() {
+    let WinLose = result();    // 2:win 1:lose 0:draw -1:continue
+    if (WinLose >= 0) {
+        /* ===== マスのクリックイベントを削除 ===== */
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                squares[x][y].removeEventListener('click', squareClick);
+            }
+        }
+        switch (WinLose) {
+            case 0:
+                messageBox.textContent = '引き分け';
+                break;
+            case 1:
+                messageBox.textContent = 'あなたの負けです';
+                break;
+            case 2:
+                messageBox.textContent = 'あなたの勝ちです';
+                break;
+        }
+        playAgain.className = 'play_again';
+        playAgain.textContent = 'もう一度プレイ';
+        gameBoard.appendChild(playAgain);
+        playAgain.addEventListener('click', resetGame);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/* ============================================================================
+    勝敗のチェック：返り値　2:win  1:lose  0:draw  -1:continue
+============================================================================ */
+function result() {
+    if (check3('valueO')) return 2;
+    if (check3('valueX')) return 1;
+    let temp = 0;
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            let e = squares[x][y].classList;
+            if (e.contains('valueO') || e.contains('valueX')) {
+                temp += 1;
+            }
+        }
+    }
+    if (temp >= 9) return 0;
+    return -1;
+}
+
+
+/* ============================================================================
+    引数で受け取ったクラスが3つ並んでいるかのチェック：揃った箇所は色をぬる
+============================================================================ */
+function check3(str) {
+    let flag = false;
+    for (let i = 0; i < 3; i++) {
+        if (squares[i][0].classList.contains(str)
+        && squares[i][1].classList.contains(str)
+        && squares[i][2].classList.contains(str)) {
+            squares[i][0].style.backgroundColor = '#FFFF99';
+            squares[i][1].style.backgroundColor = '#FFFF99';
+            squares[i][2].style.backgroundColor = '#FFFF99';
+            flag = true;
+        }
+        if (squares[0][i].classList.contains(str)
+        && squares[1][i].classList.contains(str)
+        && squares[2][i].classList.contains(str)) {
+            squares[0][i].style.backgroundColor = '#FFFF99';
+            squares[1][i].style.backgroundColor = '#FFFF99';
+            squares[2][i].style.backgroundColor = '#FFFF99';
+            flag = true;
+        }
+    }
+    if (squares[0][0].classList.contains(str)
+    && squares[1][1].classList.contains(str)
+    && squares[2][2].classList.contains(str)) {
+        squares[0][0].style.backgroundColor = '#FFFF99';
+        squares[1][1].style.backgroundColor = '#FFFF99';
+        squares[2][2].style.backgroundColor = '#FFFF99';
+        flag = true;
+    }
+    if (squares[0][2].classList.contains(str)
+    && squares[1][1].classList.contains(str)
+    && squares[2][0].classList.contains(str)) {
+        squares[0][2].style.backgroundColor = '#FFFF99';
+        squares[1][1].style.backgroundColor = '#FFFF99';
+        squares[2][0].style.backgroundColor = '#FFFF99';
+        flag = true;
+    }
+    return flag;
+}
+
+
+/* ============================================================================
+    もう一度プレイボタンを押した時の処理
+============================================================================ */
+function resetGame() {
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+            if (squares[x][y].classList.contains('valueO')) {
+                squares[x][y].classList.remove('valueO');
+            }
+            if (squares[x][y].classList.contains('valueX')) {
+                squares[x][y].classList.remove('valueX');
+            }
+            squares[x][y].style.display = 'none';
+            squares[x][y].style.backgroundColor = '#FFFFFF';
+        }
+    }
+    PlayerPlayFirst = true;
+    playAgain.remove();
+    startSelector();
 }
 
 
